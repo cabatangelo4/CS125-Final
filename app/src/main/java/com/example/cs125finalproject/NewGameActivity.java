25ddb0890d7848d5f8078fa0848a6f1eef6d82c0
cabatangelo4 <cfa2@illinois.edu>
2020-04-25 03:55:30 -0500

    Initial

---
+++ app/src/main/java/com/example/cs125finalproject/NewGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs125finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewGameActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_game);
+    }
+}
